3958d336fc835ccc2717b304778aaf577c3905a3
YisusCoreApp <jesusrobertogarcianaranjo@gmail.com>
2024-05-07 15:23:14 -0600

    ruts

--- src/app/components/inicio/inicio.component.ts
+++ src/app/components/inicio/inicio.component.ts
@@ -6,7 +6,7 @@ import { FooterComponent } from '../footer/footer.component';
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [RouterLink, InicioComponent,],
+  imports: [RouterLink,],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css'
 })
