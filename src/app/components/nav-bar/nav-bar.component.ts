c32dd1f7b269d14c35e1729b85746cd8b2fe2f25
YisusCoreApp <jesusrobertogarcianaranjo@gmail.com>
2024-06-06 17:08:15 -0600

    modificacion responsive para celulares y tabletas, inicio y detalles

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterLink, RouterOutlet } from '@angular/router';
 
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [RouterLink, RouterOutlet,],
+  imports: [RouterLink, RouterOutlet, FormsModule],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css'
 })
@@ -13,5 +14,22 @@ export class NavBarComponent {
 
   constructor(){
   }
+  
+  // menuOpen: boolean = false;
 
+  menuOpen: boolean = false;
+  dropdowns: { [key: string]: boolean } = {};
+
+  closeMenu() {
+    this.menuOpen = false;
+    this.dropdowns = {}; // Cierra todos los dropdowns
+  }
+
+  toggleDropdown(dropdown: string) {
+    this.dropdowns[dropdown] = !this.dropdowns[dropdown];
+  }
+
+  isDropdownOpen(dropdown: string): boolean {
+    return this.dropdowns[dropdown];
+  }
 }
