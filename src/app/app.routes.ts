764e1a18e0bb206149b397b03b85477926f06b17
YisusCoreApp <jesusrobertogarcianaranjo@gmail.com>
2024-06-10 17:25:36 -0600

    modificacion en el componente d afiliacion

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,7 +6,6 @@ import { AfiliacionComponent } from './components/afiliacion/afiliacion.componen
 import { HistoriaFeComponent } from './components/nosotros/historia-fe/historia-fe.component';
 import { MvvComponent } from './components/nosotros/mvv/mvv.component';
 import { CodigoEticaComponent } from './components/nosotros/codigo-etica/codigo-etica.component';
-import { EquipoTrabajoComponent } from './components/nosotros/equipo-trabajo/equipo-trabajo.component';
 import { FormularioComponent } from './components/afiliacion/formulario/formulario.component';
 import { NgModule } from '@angular/core';
 
@@ -16,7 +15,6 @@ export const routes: Routes = [
     {path: 'historia',  component: HistoriaFeComponent},
     {path: 'mvv', component: MvvComponent},
     {path: 'codigoE', component: CodigoEticaComponent},
-    {path: 'equipoT', component: EquipoTrabajoComponent},
     {path: 'recursos', component: RecursosComponent},
     {path: 'servicios', component: ServiciosComponent},
     {path: 'afiliacion', component: AfiliacionComponent},
